389d8dcf2614b45ff7a55d09e6f9524fcb1ef946
ItsOnlyBinary <git@itsonlybinary.com>
2019-04-07 22:07:52 +0100

    update deps for clean ubuntu 18.04; update build for go modules

--- ubuntu_debian_dependancies.sh
+++ ubuntu_debian_dependancies.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install -y build-essential curl git libffi-dev libfontconfig1 rpm ruby ruby-dev software-properties-common zip
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# GoLang
+sudo add-apt-repository -y ppa:longsleep/golang-backports
 
+# Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get update
-sudo apt-get install golang yarn nodejs ruby ruby-dev build-essential rpm libffi-dev
+# NodeJS
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+
+# the nodejs script above also runs apt-get update
+sudo apt-get install -y golang-go nodejs yarn
 
 sudo gem install fpm
