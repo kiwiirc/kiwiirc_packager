16379e59f288b2cd4abd54d2eb30943bf975b7d2
ItsOnlyBinary <ItsOnlyBinary@users.noreply.github.com>
2021-08-17 15:43:45 +0100

    Update scripts (#18)

--- ubuntu_debian_dependancies.sh
+++ ubuntu_debian_dependancies.sh
@@ -10,7 +10,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # NodeJS
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 
 # the nodejs script above also runs apt-get update
 sudo apt-get install -y golang-go nodejs yarn
